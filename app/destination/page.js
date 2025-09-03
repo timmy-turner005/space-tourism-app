3212b54436fdda4f7bb2e82d4642f3469e7701b1
timmy-turner005 <timitalented005@gmail.com>
2025-09-03 19:26:49 +0100

    added destination page

--- app/destination/page.js
+++ app/destination/page.js
@@ -1,5 +1,31 @@
+import { Fragment } from "react";
+
 function Destination() {
-  return <h1>Destination Page</h1>;
+  return (
+    <Fragment>
+      <p>PICK YOUR DESTINATION</p>
+      <div>
+        <span>MOON</span>
+        <span>MARS</span>
+        <span>EUROPA</span>
+        <span>TITAN</span>
+      </div>
+      <h1>MARS</h1>
+      <p>
+        Dont forget to pack your hiking boots. You will need them to tackle
+        Olympus Mons, the tallest planetary mountain in our solar system. it is
+        two and half times the size of Everest!
+      </p>
+      <section>
+        <div>
+          AVG. DISTANCE <span>225 MIL. KM</span>
+        </div>
+        <div>
+          EST. TRAVEL TIME <span>9 MONTHS</span>
+        </div>
+      </section>
+    </Fragment>
+  );
 }
 
 export default Destination;
--- app/page.js
+++ app/page.js
@@ -5,16 +5,18 @@ function HomePage() {
   return (
     <Fragment>
       <NavBar />
-      <div>
-        <p>SO, YOU WANT TO TRAVEL TO</p>
-        <p>SPACE</p>
-        <p>
-          Let us face it; if you want to go to space, you might as well
-          genuinely go to outer space and not hover kind of on the edge of it.
-          Well sit back, and relax because we will give you a truly out of this
-          world experience!
-        </p>
-      </div>
+      <section>
+        <div>
+          <p>SO, YOU WANT TO TRAVEL TO</p>
+          <h1>SPACE</h1>
+          <p>
+            Let us face it; if you want to go to space, you might as well
+            genuinely go to outer space and not hover kind of on the edge of it.
+            Well sit back, and relax because we will give you a truly out of
+            this world experience!
+          </p>
+        </div>
+      </section>
     </Fragment>
   );
 }
