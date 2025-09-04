f74bc2e5201776efe8e3492500cbd71b259fdcc5
timmy-turner005 <timitalented005@gmail.com>
2025-09-04 18:53:23 +0100

    styling homepage in progress

--- app/page.js
+++ app/page.js
@@ -1,22 +1,29 @@
 import NavBar from "@/components/Navbar";
 import { Fragment } from "react";
+import classes from "./page.module.css";
 
 function HomePage() {
   return (
     <Fragment>
       <NavBar />
-      <section>
-        <div>
-          <p>SO, YOU WANT TO TRAVEL TO</p>
-          <h1>SPACE</h1>
-          <p>
-            Let us face it; if you want to go to space, you might as well
-            genuinely go to outer space and not hover kind of on the edge of it.
-            Well sit back, and relax because we will give you a truly out of
-            this world experience!
-          </p>
-        </div>
-      </section>
+      <div className={classes.container}>
+        <main className={classes.main}>
+          <div className={classes.content}>
+            <h1 className={classes.title}>SO, YOU WANT TO TRAVEL TO</h1>
+            <h2 className={classes.space}>SPACE</h2>
+            <p className={classes.description}>
+              Let us face it; If you want to go to space, you might as well
+              genuinely go to outer space and not hover kind of on the edge of
+              it. Well sit back, and relax because we will give you a truly out
+              of this world experience!
+            </p>
+          </div>
+
+          <div className={classes["explore-container"]}>
+            <button className={classes["explore-button"]}>EXPLORE</button>
+          </div>
+        </main>
+      </div>
     </Fragment>
   );
 }
