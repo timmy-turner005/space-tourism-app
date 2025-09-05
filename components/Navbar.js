b78c3d809c98e3c5948fa7c953ebc063698ec367
timmy-turner005 <timitalented005@gmail.com>
2025-09-05 20:55:16 +0100

    navbar progressing

--- components/Navbar.js
+++ components/Navbar.js
@@ -5,20 +5,25 @@ import classes from "./navbar.module.css";
 function NavBar() {
   return (
     <Fragment>
-      <div className={classes.link}>
-        <Link href="/">
-          <span>00</span>HOME
-        </Link>
-        <Link href="/destination">
-          <span>01</span>DESTINATION
-        </Link>
-        <Link href="/crew">
-          <span>02</span>CREW
-        </Link>
-        <Link href="/technology">
-          <span>03</span>TECHNOLOGY
-        </Link>
-      </div>
+      <header className="navbar">
+        <div className="logo">✦</div>
+        <nav>
+          <ul>
+            <Link href="/">
+              <span>00</span> Home
+            </Link>
+            <Link href="/destination">
+              <span>01</span> Destination
+            </Link>
+            <Link href="/crew">
+              <span>02</span> Crew
+            </Link>
+            <Link href="/technology">
+              <span>03</span> Technology
+            </Link>
+          </ul>
+        </nav>
+      </header>
     </Fragment>
   );
 }
