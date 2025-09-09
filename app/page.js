19948494ba8044d49b8f34165341e58a88dbe5e2
timmy-turner005 <timitalented005@gmail.com>
2025-09-09 17:15:19 +0100

    homepage work

--- app/page.js
+++ app/page.js
@@ -5,24 +5,26 @@ import classes from "./page.module.css";
 function HomePage() {
   return (
     <Fragment>
-      <NavBar />
-      <div className={classes.container}>
-        <main className={classes.main}>
-          <div className={classes.content}>
-            <h1 className={classes.title}>SO, YOU WANT TO TRAVEL TO</h1>
-            <h2 className={classes.space}>SPACE</h2>
-            <p className={classes.description}>
-              Let us face it; If you want to go to space, you might as well
-              genuinely go to outer space and not hover kind of on the edge of
-              it. Well sit back, and relax because we will give you a truly out
-              of this world experience!
-            </p>
-          </div>
+      <div className={classes.background}>
+        <NavBar />
+        <div className={classes.container}>
+          <main className={classes.main}>
+            <div className={classes.content}>
+              <h1 className={classes.title}>so, you want to travel</h1>
+              <h2 className={classes.space}>space</h2>
+              <p className={classes.description}>
+                Let us face it; If you want to go to space, you might as well
+                genuinely go to outer space and not hover kind of on the edge of
+                it. Well sit back, and relax because we will give you a truly
+                out of this world experience!
+              </p>
+            </div>
 
-          <div className={classes["explore-container"]}>
-            <button className={classes["explore-button"]}>EXPLORE</button>
-          </div>
-        </main>
+            <section className={classes["explore-container"]}>
+              <div className={classes["explore-button"]}>explore</div>
+            </section>
+          </main>
+        </div>
       </div>
     </Fragment>
   );
