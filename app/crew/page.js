1c6723df656b1e8ee42f630f90ff37bf1d1a7f66
timmy-turner005 <timitalented005@gmail.com>
2025-09-11 19:21:43 +0100

    destiantion page styling

--- app/crew/page.js
+++ app/crew/page.js
@@ -1,5 +1,20 @@
+import { Fragment } from "react";
+import classes from "./crew.module.css";
+
 function Crew() {
-  return <h1>Crew Page</h1>;
+  return (
+    <Fragment>
+      <div className={classes.container}>
+        <div className={classes.container1}>
+          <h1 className={classes.head}>Crew Page</h1>
+        </div>
+
+        <div className={classes.container2}>
+          <h1 className={classes.second}>second page</h1>
+        </div>
+      </div>
+    </Fragment>
+  );
 }
 
 export default Crew;
--- app/destination/page.js
+++ app/destination/page.js
@@ -1,29 +1,46 @@
 import { Fragment } from "react";
+import classes from "./page.module.css";
+import Image from "next/image";
 
 function Destination() {
   return (
     <Fragment>
-      <p>pick your destination</p>
-      <div>
-        <span>moon</span>
-        <span>mars</span>
-        <span>europa</span>
-        <span>titan</span>
+      <div className={classes.body}>
+        <section>
+          <p className={classes.heading}>
+            <span className={classes.number}>01</span>pick your destination
+          </p>
+        </section>
+        <section>
+          <Image
+            src="/image-mars.png"
+            alt="planet"
+            width={300}
+            height={300}
+            priority
+          />
+        </section>
+        <div>
+          <span>moon</span>
+          <span>mars</span>
+          <span>europa</span>
+          <span>titan</span>
+        </div>
+        <h1>mars</h1>
+        <p>
+          Dont forget to pack your hiking boots. You will need them to tackle
+          Olympus Mons, the tallest planetary mountain in our solar system. it
+          is two and half times the size of Everest!
+        </p>
+        <section>
+          <div>
+            avg. distance <span>225 mil. km</span>
+          </div>
+          <div>
+            est. travel time <span>9 months</span>
+          </div>
+        </section>
       </div>
-      <h1>mars</h1>
-      <p>
-        Dont forget to pack your hiking boots. You will need them to tackle
-        Olympus Mons, the tallest planetary mountain in our solar system. it is
-        two and half times the size of Everest!
-      </p>
-      <section>
-        <div>
-          avg. distance <span>225 mil. km</span>
-        </div>
-        <div>
-          est. travel time <span>9 months</span>
-        </div>
-      </section>
     </Fragment>
   );
 }
