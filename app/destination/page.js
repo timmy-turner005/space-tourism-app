f84fa8403ba8b7ed82e75682a10bcfbd84ce20e2
timmy-turner005 <timitalented005@gmail.com>
2025-09-18 12:22:23 +0100

    destination page styling

--- app/destination/page.js
+++ app/destination/page.js
@@ -6,45 +6,50 @@ import NavBar from "@/components/Navbar";
 function Destination() {
   return (
     <Fragment>
-      <NavBar />
+      <div className={classes.background}>
+        <NavBar />
 
-      <section>
-        <p className={classes.heading}>
-          <span className={classes.number}>01</span>pick your destination
-        </p>
-      </section>
-      <div className={classes.divider}>
-        <section className={classes.image}>
-          <Image
-            src="/image-mars.png"
-            alt="planet"
-            width={600}
-            height={600}
-            priority
-          />
-        </section>
-
-        <section className={classes.content}>
-          <div className={classes.links}>
-            <p>moon</p>
-            <p>mars</p>
-            <p>europa</p>
-            <p>titan</p>
-          </div>
-          <h1>mars</h1>
-          <p>
-            Dont forget to pack your hiking boots. You will need them to tackle
-            Olympus Mons, the tallest planetary mountain in our solar system. it
-            is two and half times the size of Everest!
+        <section>
+          <p className={classes.heading}>
+            <span className={classes.number}>01</span>pick your destination
           </p>
-
-          <div>
-            avg. distance <span>225 mil. km</span>
-          </div>
-          <div>
-            est. travel time <span>9 months</span>
-          </div>
         </section>
+        <div className={classes.divider}>
+          <section className={classes.image}>
+            <Image
+              src="/image-mars.png"
+              alt="planet"
+              width={600}
+              height={600}
+              priority
+            />
+          </section>
+
+          <section className={classes.content}>
+            <div className={classes.links}>
+              <p>moon</p>
+              <p>mars</p>
+              <p>europa</p>
+              <p>titan</p>
+            </div>
+            <h1 className={classes.mars}>mars</h1>
+            <p className={classes.text}>
+              Dont forget to pack your hiking boots. You will need them to
+              tackle Olympus Mons, the tallest planetary mountain in our solar
+              system. it is two and half times the size of Everest!
+            </p>
+            <footer className={classes.footer}>
+              <div className={classes.distance}>
+                <p className={classes.footone}>avg. distance</p>
+                <p>225 mil. km</p>
+              </div>
+              <div className={classes.distance}>
+                <p className={classes.footone}>est. travel time</p>
+                <p>9 months</p>
+              </div>
+            </footer>
+          </section>
+        </div>
       </div>
     </Fragment>
   );
