ad27c836c88308ceeeed96a163283f2e35703908
timmy-turner005 <timitalented005@gmail.com>
2025-09-22 16:43:48 +0100

    restyling homepage

--- app/page.js
+++ app/page.js
@@ -1,11 +1,13 @@
 import NavBar from "@/components/Navbar";
 import { Fragment } from "react";
+import Image from "next/image";
 import classes from "./page.module.css";
+import Link from "next/link";
 
 function HomePage() {
   return (
     <Fragment>
-      <div className={classes.background}>
+      {/* <div className={classes.background}>
         <NavBar />
         <div className={classes.container}>
           <main className={classes.main}>
@@ -25,6 +27,49 @@ function HomePage() {
             </section>
           </main>
         </div>
+      </div> */}
+      <div className={classes.background}>
+        <section className={classes.navbar}>
+          <div className={classes.logo}>
+            <Image
+              src="/logo.svg"
+              alt="star-logo"
+              width={50}
+              height={50}
+              priority
+            />
+          </div>
+          <div className={classes.links}>
+            <Link href="/" className={classes.navlink}>
+              <span>00</span>home
+            </Link>
+            <Link href="/destination" className={classes.navlink}>
+              <span>01</span>destination
+            </Link>
+            <Link href="/crew" className={classes.navlink}>
+              <span>02</span>crew
+            </Link>
+            <Link href="/technology" className={classes.navlink}>
+              <span>03</span>technology
+            </Link>
+          </div>
+        </section>
+
+        <section>
+          <div>
+            <p>so you want to travel to sapce</p>
+            <h1>space</h1>
+            <p>
+              Let&apos;s face it; If you want to go to space, you might as well
+              genuinely go to outer space and not hover kind of on the edge of
+              it. Well sit back, and relax because we&apos;ll give you a truly
+              out of this world experience!
+            </p>
+          </div>
+          <div>
+            <p>explore</p>
+          </div>
+        </section>
       </div>
     </Fragment>
   );
