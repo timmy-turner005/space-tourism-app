bc2883f13970425c9ba4f8a3545ba6d072cc6c60
timmy-turner005 <timitalented005@gmail.com>
2025-09-24 12:15:58 +0100

    almost done

--- app/page.js
+++ app/page.js
@@ -41,7 +41,7 @@ function HomePage() {
           </div>
           <div className={classes.links}>
             <Link href="/" className={classes.navlink}>
-              <span>00</span>home
+              <span className={classes.nums}>00</span>home
             </Link>
             <Link href="/destination" className={classes.navlink}>
               <span>01</span>destination
@@ -57,9 +57,9 @@ function HomePage() {
 
         <section className={classes.content}>
           <div className={classes["text-content"]}>
-            <p>so you want to travel to sapce</p>
-            <h1>space</h1>
-            <p>
+            <p className={classes["first-text"]}>so, you want to travel to</p>
+            <h1 className={classes["second-text"]}>space</h1>
+            <p className={classes["third-text"]}>
               Let&apos;s face it; If you want to go to space, you might as well
               genuinely go to outer space and not hover kind of on the edge of
               it. Well sit back, and relax because we&apos;ll give you a truly
