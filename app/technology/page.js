ca07c718aa5686f13002d42caaaf25df7196c908
timmy-turner005 <timitalented005@gmail.com>
2025-10-02 11:29:31 +0100

    technology page

--- app/technology/page.js
+++ app/technology/page.js
@@ -1,5 +1,38 @@
+import { Fragment } from "react";
+import classes from "./page.module.css";
+import Image from "next/image";
+import Link from "next/link";
+
 function Technology() {
-  return <h1>Technology Page</h1>;
+  return (
+    <Fragment>
+      <section className={classes.navbar}>
+        <div className={classes.logo}>
+          <Image
+            src="/logo.svg"
+            alt="star-logo"
+            width={50}
+            height={50}
+            priority
+          />
+        </div>
+        <div className={classes.links}>
+          <Link href="/" className={classes.navlink}>
+            <span className={classes.nums}>00</span>home
+          </Link>
+          <Link href="/destination" className={classes.navlink}>
+            <span>01</span>destination
+          </Link>
+          <Link href="/crew" className={classes.navlink}>
+            <span>02</span>crew
+          </Link>
+          <Link href="/technology" className={classes.navlink}>
+            <span>03</span>technology
+          </Link>
+        </div>
+      </section>
+    </Fragment>
+  );
 }
 
 export default Technology;
