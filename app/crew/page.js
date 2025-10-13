caf07d0972f4412266002d5fdc72a54689853bf8
timmy-turner005 <timitalented005@gmail.com>
2025-10-13 18:03:45 +0100

    another commit

--- app/crew/page.js
+++ app/crew/page.js
@@ -1,65 +1,65 @@
-import { Fragment } from "react";
-import classes from "./page.module.css";
-import Image from "next/image";
-import Link from "next/link";
+// import { Fragment } from "react";
+// import classes from "./page.module.css";
+// import Image from "next/image";
+// import Link from "next/link";
 
-function Crew() {
-  return (
-    <Fragment>
-      <section className={classes.navbar}>
-        <div className={classes.logo}>
-          <Image
-            src="/logo.svg"
-            alt="star-logo"
-            width={50}
-            height={50}
-            priority
-          />
-        </div>
-        <div className={classes.links}>
-          <Link href="/" className={classes.navlink}>
-            <span className={classes.nums}>00</span>home
-          </Link>
-          <Link href="/destination" className={classes.navlink}>
-            <span>01</span>destination
-          </Link>
-          <Link href="/crew" className={classes.navlink}>
-            <span>02</span>crew
-          </Link>
-          <Link href="/technology" className={classes.navlink}>
-            <span>03</span>technology
-          </Link>
-        </div>
-      </section>
+// function Crew() {
+//   return (
+//     <Fragment>
+//       <section className={classes.navbar}>
+//         <div className={classes.logo}>
+//           <Image
+//             src="/logo.svg"
+//             alt="star-logo"
+//             width={50}
+//             height={50}
+//             priority
+//           />
+//         </div>
+//         <div className={classes.links}>
+//           <Link href="/" className={classes.navlink}>
+//             <span className={classes.nums}>00</span>home
+//           </Link>
+//           <Link href="/destination" className={classes.navlink}>
+//             <span>01</span>destination
+//           </Link>
+//           <Link href="/crew" className={classes.navlink}>
+//             <span>02</span>crew
+//           </Link>
+//           <Link href="/technology" className={classes.navlink}>
+//             <span>03</span>technology
+//           </Link>
+//         </div>
+//       </section>
 
-      <div>
-        <p>02</p>
-        <p>meet your crew</p>
-      </div>
+//       <div>
+//         <p>02</p>
+//         <p>meet your crew</p>
+//       </div>
 
-      <section>
-        <div>
-          <p>flight engineer</p>
-          <h1>anousheh ansari</h1>
-          <p>
-            Anousheh Ansari is an Iranian American engineer and co-founder of
-            Prodea Systems. Ansari was the fourth self-funded space tourist, the
-            first self-funded woman to fly to the ISS, and the first Iranian in
-            space.
-          </p>
-        </div>
-        <div>
-          <Image
-            src="/image-anousheh-ansari.png"
-            alt="image of Anousheh Ansari"
-            width={300}
-            height={300}
-            priority
-          />
-        </div>
-      </section>
-    </Fragment>
-  );
-}
+//       <section>
+//         <div>
+//           <p>flight engineer</p>
+//           <h1>anousheh ansari</h1>
+//           <p>
+//             Anousheh Ansari is an Iranian American engineer and co-founder of
+//             Prodea Systems. Ansari was the fourth self-funded space tourist, the
+//             first self-funded woman to fly to the ISS, and the first Iranian in
+//             space.
+//           </p>
+//         </div>
+//         <div>
+//           <Image
+//             src="/image-anousheh-ansari.png"
+//             alt="image of Anousheh Ansari"
+//             width={300}
+//             height={300}
+//             priority
+//           />
+//         </div>
+//       </section>
+//     </Fragment>
+//   );
+// }
 
-export default Crew;
+// export default Crew;
