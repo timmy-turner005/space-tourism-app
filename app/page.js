d4dc9408b28012bd52f1db127bcbb7b8ac7964ad
timmy-turner005 <timitalented005@gmail.com>
2025-10-21 17:27:28 +0100

    homepage cleansing

--- app/page.js
+++ app/page.js
@@ -7,27 +7,6 @@ import Link from "next/link";
 function HomePage() {
   return (
     <Fragment>
-      {/* <div className={classes.background}>
-        <NavBar />
-        <div className={classes.container}>
-          <main className={classes.main}>
-            <div className={classes.content}>
-              <h1 className={classes.title}>so, you want to travel</h1>
-              <h2 className={classes.space}>space</h2>
-              <p className={classes.description}>
-                Let us face it; If you want to go to space, you might as well
-                genuinely go to outer space and not hover kind of on the edge of
-                it. Well sit back, and relax because we will give you a truly
-                out of this world experience!
-              </p>
-            </div>
-
-            <section className={classes["explore-container"]}>
-              <div className={classes["explore-button"]}>explore</div>
-            </section>
-          </main>
-        </div>
-      </div> */}
       <div className={classes.background}>
         <section className={classes.navbar}>
           <div className={classes.logo}>
